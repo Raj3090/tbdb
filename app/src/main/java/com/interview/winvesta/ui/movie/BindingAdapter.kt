7e8361eda4f98c32e53324a006dac39cc7cb277e
raj <raj.kumar1@zunroof.com>
2021-05-09 19:47:52 +0530

    add binding adapter for image loading

--- app/src/main/java/com/interview/winvesta/ui/movie/BindingAdapter.kt
+++ app/src/main/java/com/interview/winvesta/ui/movie/BindingAdapter.kt
@@ -4,13 +4,20 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.interview.winvesta.BuildConfig
+import com.interview.winvesta.data.api.MovieService
+import com.interview.winvesta.utils.IMAGE_BASE_URL
+import com.interview.winvesta.utils.IMAGE_FILE_SIZE
 
 
 @BindingAdapter("imageFromUrl")
     fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
-        if (!imageUrl.isNullOrEmpty()) {
+
+     val image = IMAGE_BASE_URL + IMAGE_FILE_SIZE + imageUrl
+
+    if (!imageUrl.isNullOrEmpty()) {
             Glide.with(view.context)
-                .load(imageUrl)
+                .load(image)
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(view)
         }
