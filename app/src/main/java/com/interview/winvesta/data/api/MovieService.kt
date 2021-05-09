8f4476359d9f5337004d6b2723447a5397b15200
raj <raj.kumar1@zunroof.com>
2021-05-09 13:57:30 +0530

    add tmdb api service using retrofit for network calls

---
+++ app/src/main/java/com/interview/winvesta/data/api/MovieService.kt
@@ -0,0 +1,41 @@
+package com.interview.winvesta.data.api
+
+import com.interview.winvesta.BuildConfig
+import com.interview.winvesta.data.model.PopularMovieResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Used to connect to the The movie DB API to fetch photos
+ */
+interface MovieService {
+    @GET("discover/movie")
+    suspend fun getPopularMovies(
+        @Query("sort_by") query: String,
+        @Query("page") page: Int,
+        @Query("api_key") clientId: String = BuildConfig.MOVIE_API_ACCESS_KEY
+    ): PopularMovieResponse
+
+    companion object {
+        private const val BASE_URL = "https://api.themoviedb.org/3/"
+
+        fun create(): MovieService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MovieService::class.java)
+        }
+    }
+}
--- app/src/main/java/com/interview/winvesta/data/repository/MoviesRepository.kt
+++ app/src/main/java/com/interview/winvesta/data/repository/MoviesRepository.kt
@@ -1,5 +1,5 @@
 package com.interview.winvesta.data.repository
 
-class MoviesRepository {
+class MoviesRepository() {
 
 }
